06ebbb2aae11e38d252d454474ddaa9a0abbcbd8
Hadis1990 <hsteammsteam@gmail.com>
2025-03-10 17:21:09 +0100

    add json tags to user model

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	ID int
-	Username string
-	Email string
-	Password string
+	ID int `json:"id"`
+	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"-"`
 }
